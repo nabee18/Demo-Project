7d2c15a430217aba6419a91649657bedc1560d01
Taliq-nd <nguepingdassi@iut-dhaka.edu>
2026-08-05 18:19:28 +0600

    Declared expected CGPA calculator

--- modules/gpa.h
+++ modules/gpa.h
@@ -10,6 +10,6 @@ extern const char *gradeLetters[];
 double getGradePoint(CourseResult result);
 char *getLetterGrade(CourseResult result);
 double calculateGPA(CourseResult results[], int n_results);
-double calculateRequiredGPA(double current_cgpa, double completed_credits, double target_cgpa, double remaining_credits);
+double calculateExpectedCGPA(double current_cgpa, double completed_credits, double expected_cgpa, double future_credits);
 
 #endif
